fe2295aef82f28c76f8c73389dd1e33f31eb2072
jdeking14 <javivi@gmail.com>
2024-05-06 23:14:30 +0200

    subida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/Login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: LoginComponent },//TODO: Poner aqui la pagina del home. esto es solo una prueba
+  { path: 'login', component: LoginComponent },
+  { path: "**", redirectTo: 'home' }
+];
---
+++ src/app/components/Login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  //TODO: Meter logica de login , apunte a componente registro y a olvide mi contraseña
+
+  constructor(
+   
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  
+}
