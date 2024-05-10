bcf4f49d5be3a4174b07dbdbebb14ee89a7b973d
Jaime Amate R2D <jaime_amate@roadtodata.com>
2024-05-10 13:16:36 +0200

    vista de grupos creada y ruta /home asignada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/Login/login.component';
+import { ListaGruposComponent } from './components/lista-grupos/lista-grupos.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: LoginComponent },//TODO: Poner aqui la pagina del home. esto es solo una prueba
+  { path: 'home', component: ListaGruposComponent },
   { path: 'login', component: LoginComponent },
   { path: "**", redirectTo: 'home' }
 ];
