a0cfa6f56273d5e8d5c2f9028ea9a14f5a1b40d6
Jaime Amate R2D <jaime_amate@roadtodata.com>
2024-05-26 13:00:37 +0200

    created group/:id route path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/Login/login.component';
 import { ListaGruposComponent } from './components/lista-grupos/lista-grupos.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { GrupoViewComponent } from './components/grupo-view/grupo-view.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: ListaGruposComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistroComponent },
+  { path: 'group/:id', component: GrupoViewComponent},
   { path: "**", redirectTo: 'home' }
 ];
