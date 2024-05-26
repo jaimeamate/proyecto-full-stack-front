1bcc51921040c41dcbb7d98ae389ddacdede5688
Jaime Amate R2D <jaime_amate@roadtodata.com>
2024-05-26 23:27:21 +0200

    basic group component services added

--- src/app/components/lista-grupos/lista-grupos.component.ts
+++ src/app/components/lista-grupos/lista-grupos.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { GrupoItemComponent } from '../grupo-item/grupo-item.component';
+import { GroupService } from '../../services/group.service';
+import { IGroup } from '../../interfaces/igroup';
 
 @Component({
   selector: 'app-lista-grupos',
@@ -10,5 +12,16 @@ import { GrupoItemComponent } from '../grupo-item/grupo-item.component';
   styleUrl: './lista-grupos.component.css'
 })
 export class ListaGruposComponent {
-  listaGrupos = ['Grupo 1','Grupo 2','Grupo 3','Grupo 4','Grupo 5','Grupo 6','Grupo 7','Grupo 8','Grupo 9','Grupo 10']
+  groupService = inject(GroupService)
+  groups: IGroup[] = []
+  
+  async getGroups() {
+    this.groups = await this.groupService.getAll()
+    console.log(this.groups)
+  }
+
+  ngOnInit() {
+    this.getGroups()
+  }
+  
 }
