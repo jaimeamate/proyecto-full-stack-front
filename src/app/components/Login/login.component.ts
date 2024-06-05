b43b245ab5a71a14f1364c24b76eb1e1ce910eb8
jdeking14 <>
2024-06-05 20:57:11 +0200

    subida para correcto login

--- src/app/components/Login/login.component.ts
+++ src/app/components/Login/login.component.ts
@@ -1,22 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { UsuariosService } from '../../services/user.service';
+import { FormsModule, NgModel } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  //TODO: Meter logica de login , apunte a componente registro y a olvide mi contraseña
+  credentials = {
+    email: '',
+    password: ''
+  };
 
-  constructor(
-   
-  ) {}
-
+  constructor(private usuariosService: UsuariosService, private router: Router) { }
   ngOnInit(): void {
-    
+    throw new Error('Method not implemented.');
+  }
+
+  onSubmit() {
+    this.usuariosService.login(this.credentials).subscribe(
+      response => {
+        // Manejo de la respuesta del login
+        console.log('Login exitoso:', response);
+        // Redirigir a la página principal o a otra página después del login
+        this.router.navigate(['/home']);
+      },
+      error => {
+        // Manejo de errores
+        console.error('Error en el login:', error);
+        alert('Usuario o contraseña incorrectos');
+      }
+    );
   }
 
   
