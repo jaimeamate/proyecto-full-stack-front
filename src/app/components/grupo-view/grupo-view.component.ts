699f073bec48184edf45168c64d8c95de5681459
Jaime Amate R2D <jaime_amate@roadtodata.com>
2024-06-05 17:07:22 +0200

    commit test

--- src/app/components/grupo-view/grupo-view.component.ts
+++ src/app/components/grupo-view/grupo-view.component.ts
@@ -1,28 +1,45 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { GroupService } from '../../services/group.service';
 import { IGroup } from '../../interfaces/igroup';
+import { GastosListComponent } from '../gastos-list/gastos-list.component';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-grupo-view',
   standalone: true,
-  imports: [],
+  imports: [GastosListComponent,NgbDropdownModule,FormsModule],
   templateUrl: './grupo-view.component.html',
   styleUrl: './grupo-view.component.css'
 })
 export class GrupoViewComponent {
   activatedRoute = inject(ActivatedRoute)
+  router = inject(Router)
   groupService = inject(GroupService)
-  group: IGroup | any
+  group!: IGroup;
+  groupBase!: IGroup;
+  editing: boolean = true;
   
   ngOnInit() {
     this.activatedRoute.params.subscribe(async (params:any) => {
       const id = params.id
       try {
-        this.group = await this.groupService.getById(id)
+        this.groupBase = await this.groupService.getById(id)
+        this.group = this.groupBase;
       } catch (error) {
-        console.log(error)
+        this.router.navigate(['/home'])
       }
     })
+  }
+
+  editMode() {
+    this.editing = !this.editing
+  }
+
+  editGroup(): boolean | void {
+    if (this.group.name === undefined) return false
+    this.groupService.updateById(this.group)
+    this.editMode()
   }
 }
