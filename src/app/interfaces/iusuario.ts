6ac6720a3077497dd19855afe4c16b8165c387ef
jdeking14 <>
2024-06-05 20:52:00 +0200

    subida de api usuarios

---
+++ src/app/interfaces/iusuario.ts
@@ -0,0 +1,11 @@
+export interface Usuario {
+    _id: string;
+    id: number;
+    first_name: string;
+    last_name: string;
+    username: string;
+    email: string;
+    image: string;
+    password: string;
+  }
+  
