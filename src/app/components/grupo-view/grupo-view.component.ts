887bb7286a5a68d5f710982f1ca4765cb33e5aed
Jaime Amate R2D <jaime_amate@roadtodata.com>
2024-06-08 13:28:23 +0200

    feature: edit group name & description

--- src/app/components/grupo-view/grupo-view.component.ts
+++ src/app/components/grupo-view/grupo-view.component.ts
@@ -4,7 +4,7 @@ import { GroupService } from '../../services/group.service';
 import { IGroup } from '../../interfaces/igroup';
 import { GastosListComponent } from '../gastos-list/gastos-list.component';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-grupo-view',
@@ -17,16 +17,17 @@ export class GrupoViewComponent {
   activatedRoute = inject(ActivatedRoute)
   router = inject(Router)
   groupService = inject(GroupService)
-  group!: IGroup;
-  groupBase!: IGroup;
-  editing: boolean = true;
+  group: IGroup = { 
+    "name": '',
+    "description": ''
+  };
+  editing: boolean = false;
   
   ngOnInit() {
     this.activatedRoute.params.subscribe(async (params:any) => {
       const id = params.id
       try {
-        this.groupBase = await this.groupService.getById(id)
-        this.group = this.groupBase;
+        this.group = await this.groupService.getById(id)
       } catch (error) {
         this.router.navigate(['/home'])
       }
@@ -38,8 +39,13 @@ export class GrupoViewComponent {
   }
 
   editGroup(): boolean | void {
-    if (this.group.name === undefined) return false
     this.groupService.updateById(this.group)
     this.editMode()
+  }
+
+  applyEdit(form: NgForm){
+    form.value.id = this.group.id
+    this.group = form.value;
+    this.editGroup()
   }
 }
